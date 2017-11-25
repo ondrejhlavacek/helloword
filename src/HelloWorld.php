e22b3ff4592ec5f7ebb8373d96b81e943f9e434f
dominikvisek <visekdo@gmail.com>
2017-11-25 12:31:15 +0100

    hello without test

--- src/HelloWorld.php
+++ src/HelloWorld.php
@@ -4,4 +4,27 @@ namespace PosledniSobota;
 
 class HelloWorld
 {
+
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * HelloWorld constructor.
+     * @param string $name
+     */
+    public function __construct($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function sayHello()
+    {
+        return 'Hello ' . $this->name;
+    }
+
 }
